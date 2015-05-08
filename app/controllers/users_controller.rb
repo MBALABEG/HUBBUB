d140033278a73f6c9171729c4ef406c242d56acb
Aldoreino <alberto.abraham.castillo@gmail.com>
2015-05-07 18:15:11 -0700

    sessions controller and users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,16 @@
 class UsersController < ApplicationController
-	
+	def new
+		@user = User.new
+	end
+	def create
+		@user = User.new(user_params)
+		if @user.save # creates a new session when a user logs in 
+			session[:user_id] = @user.id
+			redirect_to '/'
+		end
+	end
+	private
+	def user_params # users are required to have these attributes
+		require(:user).permit(:name, :email, :password)
+	end
 end
