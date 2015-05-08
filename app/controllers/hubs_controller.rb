f9d55766a019805dd4eaf8f921b07d7b3a9a2b38
Alex Risberg <alex.risberg@gmail.com>
2015-05-08 09:51:59 -0700

    creates create

--- app/controllers/hubs_controller.rb
+++ app/controllers/hubs_controller.rb
@@ -5,4 +5,14 @@ class HubsController < ApplicationController
         @insta = Instagram.tag_recent_media(tag)
         @twitter = $twitter_client.search("#" + tag).take(20)
     end
+
+    def create
+        store = []
+        @insta.each do |gram|
+            store.push(gram.id)
+        end
+    end
+
 end
+
+
