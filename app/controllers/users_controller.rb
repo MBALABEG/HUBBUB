17d3d8f4926a4e7f28ecb82b72dd2411e248a370
Aldoreino <alberto.abraham.castillo@gmail.com>
2015-05-08 17:21:01 -0700

    view for sign up and users can be created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,12 @@ class UsersController < ApplicationController
 		if @user.save # creates a new session when a user logs in 
 			session[:user_id] = @user.id
 			redirect_to '/'
+		else
+			redirect_to '/'
 		end
 	end
 	private
 	def user_params # users are required to have these attributes
-		require(:user).permit(:name, :email, :password)
+		params.require(:user).permit(:name, :email, :password)
 	end
 end
