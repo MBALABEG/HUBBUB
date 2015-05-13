f91a431bd0fc767721752c2637b8dea9a12dcecf
Aldoreino <alberto.abraham.castillo@gmail.com>
2015-05-12 17:31:59 -0700

    users can sign in and confirm password

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root "welcome#index"
   get "/hubs/new" => "hubs#new"
-  get "/signup" => "users#new"
+  get "users/signup" => "users#new"
   post "/users" => "users#create"
 
 end
