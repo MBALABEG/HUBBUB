49e7db0c609230408a1b377f4ca3896b5bfc7442
czeins <claude.zeins@gmail.com>
2015-05-14 15:36:22 -0700

    Added hubs api

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,19 @@ Rails.application.routes.draw do
 
       root "welcome#index"
 
-      resources :hubs, :only => [:new, :create, :show]
-
       get "/signup" => "users#new"
       post "/users" => "users#create"
 
       get "sessions/login" => "sessions#new"
       post "/login" => "sessions#create"
+
+    resources :hubs, :only => [:new, :create, :show]
+
+      namespace :api, :only => [:show], defaults: { format: :json } do
+        namespace :v1 do
+            resources :hubs, :only => [:new, :create, :show]
+        end
+      end
+
 
 end
