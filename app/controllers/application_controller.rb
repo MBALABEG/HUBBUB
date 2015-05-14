d303e7093a62c2c5ec5db81ab5a031cf4169aa8f
Aldoreino <alberto.abraham.castillo@gmail.com>
2015-05-14 16:38:03 -0700

    users are displayed and can log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  helper_method :current_user # tells the application that current_user is a helper method
+
+
+
+  def current_user # method for current_user that displays the users name on the page
+  	if session[:user_id]
+  		@current_user||= User.find(session[:user_id])
+  		return @current_user
+  	else
+  		return nil
+  	end
+  end
 end
